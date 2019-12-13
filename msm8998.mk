03c3d1c36b72b261dcb85b9b7a44981d394bfbf6
moghn <1036664317@qq.com>
2019-12-13 22:01:36 +0800

    nitrogen settings chinese

--- msm8998.mk
+++ msm8998.mk
@@ -89,12 +89,11 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# Alipay
-#PRODUCT_PACKAGES += \
-    IFAAService \
+Alipay
+PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-#PRODUCT_BOOT_JARS += \
+PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
 # Audio
